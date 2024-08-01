04bfe8d294262833af434700af1ecc2957d883d9
Daniel <162492997+DanielCarvalhoS@users.noreply.github.com>
2024-08-01 12:15:01 -0300

    Add files via upload

---
+++ flappy/js/classes.js
@@ -0,0 +1,16 @@
+class obj {
+    constructor(posx,posy,largura,altura,imagem){
+        this.posx = posx
+        this.posy = posy
+        this.largura = largura
+        this.altura = altura
+        this.imagem = imagem
+    }
+}
+
+draw(){
+    let img = new image()
+}
+
+
+    
