5376b7e9afd08ec7e0b5ad0bb87a179c0459af9c
DanielCarvalhoS <carvalhodan07@gmail.com>
2024-08-07 10:42:38 -0300

    aula

---
+++ js/classes.js
@@ -0,0 +1,27 @@
+class obj {
+    constructor(posx,posy,largura,altura,imagem){
+        this.posx = posx
+        this.posy = posy
+        this.largura = largura
+        this.altura = altura
+        this.imagem = imagem
+    }
+
+
+draw(){
+    let img = new Image()
+    img.src = this.imagem
+    canvas.drawImage(img,this.posx,this.posy,this.largura,this.altura)
+    }
+}
+
+class Bg extends obj{
+    move(velocidade,limite,posicao){
+        this.posx -= velocidade
+    if(this.posx < limite){
+        this.posx = posicao
+        }
+    }
+}
+
+    
