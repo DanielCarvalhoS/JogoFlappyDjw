450fb095abf57638ea9b36e705a92a9b03edb0f5
DanielCarvalhoS <carvalhodan07@gmail.com>
2024-09-04 10:40:40 -0300

    aa

--- js/classes.js
+++ js/classes.js
@@ -1,4 +1,6 @@
 class obj {
+    quadro = 0
+    tempo = 0
     constructor(posx,posy,largura,altura,imagem){
         this.posx = posx
         this.posy = posy
@@ -13,6 +15,19 @@ draw(){
     img.src = this.imagem
     canvas.drawImage(img,this.posx,this.posy,this.largura,this.altura)
     }
+
+
+animacao(vel, limite, nomeImg){
+        this.tempo += 1
+        if(this.tempo >= vel){
+            this.tempo = 0
+            this.quadro += 1
+        }
+        if(this.quadro >= limite){
+            this.quadro = 0
+        }
+        this.imagem = 'img/'+nomeImg+this.quadro+'.png'
+    }
 }
 
 class Bg extends obj{
@@ -24,4 +39,29 @@ class Bg extends obj{
     }
 }
 
+class Ground extends Bg{
+
+}
+
+class Canu extends obj{
     
+}
+class Bird extends obj{
+    velocidade = 2 
+    gravidade = 1
+    move(){
+        if(this.velocidade < 10){
+            this.velocidade += this.gravidade
+        }
+        this.posy += this.velocidade        
+    }
+    limite(){
+        if(this.posy >= 660){
+                this.posy = 660
+            }
+        else if(this.posy <= 110){
+            this.posy = 110
+        }
+    }     
+}
+
--- js/script.js
+++ js/script.js
@@ -2,29 +2,47 @@ const canvas = document.getElementById('canvas').getContext('2d')
 
 let bg = new Bg(0,0,500,900,'img/sky.png')
 let bg2 = new Bg(500,0,500,900,'img/sky.png')
-let ground = new Bg(0,700,500,200,'img/ground.png')
-let ground2 = new Bg(500,700,500,200,'img/ground.png')
+let ground = new Ground(0,700,500,200,'img/ground.png')
+let ground2 = new Ground(500,700,500,200,'img/ground.png')
+let cano = new Canu (300,500,96,358,'img/pipe1.png' )
+let cano2 = new Canu (300,0,96,358,'img/pipe2.png' )
+let bird = new Bird (50, 400, 63, 51, 'img/bird0.png')
+
+document.addEventListener('click', function(e){
+    bird.velocidade -= 12
+})
 
 function draw(){
     bg.draw()
     bg2.draw()
+    cano.draw()
+    cano2.draw()
     ground.draw()
     ground2.draw()
+    
+    bird.draw()
 }
 
+
 function update(){
-    bg.move(10, -500, 0)
-    bg2.move(10, 0, 500)
-    ground.move(10,-500, 0)
-    ground2.draw(10, 0, 500)
+    bg.move(1, -500, 0)
+    bg2.move(1, 0, 500)
+    ground.move(2,-500, 0)
+    ground2.move(2, 0, 500)
+    bird.limite() 
+    bird.move()
+    bird.animacao(3, 4, 'bird')
 }
+
+
 
 //Chamava as outras funções, porém antes de atualizar, ela limpava a tela para desenhar, etc...
 function main(){
     canvas.clearRect(0,0,500,900)
     update()
     draw()  
+    requestAnimationFrame(main)
 }
 
-setInterval(main, 100)
+main()
 
